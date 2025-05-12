5f4496f075fbcdf864d76d04ade301f61af7ccb1
minhpv-3059 <phan.van.minh@sun-asterisk.com>
2025-05-12 08:23:49 +0700

    fix: update UserAdminController to AdminController

--- src/main/java/com/sun/wineshop/controller/admin/AdminController.java
+++ src/main/java/com/sun/wineshop/controller/admin/AdminController.java
@@ -3,30 +3,27 @@ package com.sun.wineshop.controller.admin;
 import com.sun.wineshop.dto.response.BaseApiResponse;
 import com.sun.wineshop.dto.response.UserResponse;
 import com.sun.wineshop.service.UserService;
-import com.sun.wineshop.utils.AppConstants;
 import com.sun.wineshop.utils.api.AdminApiPaths;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(AdminApiPaths.User.BASE)
-@PreAuthorize("hasRole('" + AppConstants.ADMIN + "')")
-public class UserAdminController {
+@RequestMapping(AdminApiPaths.BASE)
+public class AdminController {
 
     private final UserService userService;
 
-    @GetMapping
+    @GetMapping(AdminApiPaths.User.BASE)
     public ResponseEntity<Page<UserResponse>> getAllUsers(Pageable pageable) {
         return ResponseEntity.ok(userService.getAllUsers(pageable));
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(AdminApiPaths.User.BY_ID)
     public ResponseEntity<BaseApiResponse<UserResponse>> getUserById(@PathVariable Long id) {
         return ResponseEntity.ok(new BaseApiResponse<>(
                 HttpStatus.OK.value(),
--- src/main/java/com/sun/wineshop/utils/api/AdminApiPaths.java
+++ src/main/java/com/sun/wineshop/utils/api/AdminApiPaths.java
@@ -5,13 +5,13 @@ public class AdminApiPaths {
     public static final String BASE_ALL = BASE + "/**";
 
     public static final class User {
-        public static final String BASE = AdminApiPaths.BASE + "/users";
+        public static final String BASE = "/users";
         public static final String BY_ID = BASE + "/{id}";
         public static final String SEARCH = BASE + "/search";
     }
 
     public static final class Product {
-        public static final String BASE = AdminApiPaths.BASE + "/products";
+        public static final String BASE = "/products";
         public static final String BY_ID = BASE + "/{id}";
         // ...
     }
