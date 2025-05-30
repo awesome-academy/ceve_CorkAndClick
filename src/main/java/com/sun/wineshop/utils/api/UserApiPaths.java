6b8321b980734fc8e9caa2e119deb473827fcf1f
minhpv-3059 <phan.van.minh@sun-asterisk.com>
2025-05-30 16:57:27 +0700

    feat: Add feature chat with admin by socket

--- src/main/java/com/sun/wineshop/utils/api/UserApiPaths.java
+++ src/main/java/com/sun/wineshop/utils/api/UserApiPaths.java
@@ -10,4 +10,12 @@ public class UserApiPaths {
         public static final String FULL_INFO = BASE +  INFO;
         public static final String FULL_REGISTER = BASE + REGISTER;
     }
+
+    public static class Chat {
+        public static final String BASE = UserApiPaths.BASE + "/chat";
+        public static final String HISTORY = "/history";
+        public static final String WEBSOCKET_ENDPOINT = "/ws/chat";
+        public static final String WEBSOCKET_BROKER = "/topic/messages/";
+        public static final String SEND = "/chat.send.user";
+    }
 }
